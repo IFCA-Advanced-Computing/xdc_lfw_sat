81aa1d59ebd55693ab43bca751aa91add04e8753
garciadd <garciad@ifca.unican.es>
2020-02-07 09:07:53 +0100

    Update xdc_lfw_sat.py

--- sat_server/xdc_lfw_sat.py
+++ sat_server/xdc_lfw_sat.py
@@ -8,104 +8,69 @@ from sat_modules import download_landsat
 
 parser = argparse.ArgumentParser(description='Gets data from satellite')
 
-parser.add_argument("-sd",
-                    "--startdate",
-                    help="The Start Date - format DD-MM-YYYY",
-                    required=True,
-                    dest='start_date')
-
-parser.add_argument("-ed",
-                    "--enddate",
-                    help="The Start Date - format DD-MM-YYYY",
-                    required=True,
-                    dest='end_date')
-
-parser.add_argument("-reg",
-                   "--region",
-                   help = "Name of the region selected",
-                   required=True,)
-
-
-parser.add_argument('--coord',
-                    required=False,
-                    help='list of coordinates')
-
-parser.add_argument('--sat',
-		    help="Sentinel2 or Landsat8",
-		    required=False,
-		    choices=['Sentinel2', 'Landsat8', 'All'])
-
-parser.add_argument('--cloud',
-            help="Maximum percentage of cloud",
-            required=False,)
-
-parser.add_argument('-path',
-		   help='output path',
-		   required=True)
-
+parser.add_argument("-m", "--mydict", action="store",
+                    required=False, type=str)
 args = parser.parse_args()
+sat_args = json.loads(args.mydict)
 
 #Check the format date and if end_date > start_date
-sd, ed = utils.valid_date(args.start_date, args.end_date)
-
-#chek the region to attach coordinates
-coord = utils.valid_region(args.region, args.coord)
+sd, ed = utils.valid_date(sat_args['start_date'], sat_args['end_date'])
 
 #configure the tree of datasets path
-utils.configuration_path(args.path, args.region)
+utils.configuration_path(sat_args['sat_path'], sat_args['region'])
 
-if args.sat == "Sentinel2":
+if sat_args['sat_type'] == "Sentinel2":
 
     s2_credentials = config.sentinel_pass
 
-    S2_args = {'inidate':sd,
-               'enddate':ed,
-               'region':args.region,
-               'coordinates': coord,
-               'platform':'Sentinel-2',
-               'producttype':'S2MSI1C',
-               'cloud': args.cloud,
-               'username':s2_credentials['username'],
+    S2_args = {'inidate': sd,
+               'enddate': ed,
+               'region': sat_args['region'],
+               'coordinates': sat_args['coordinates'],
+               'platform': 'Sentinel-2',
+               'producttype': 'S2MSI1C',
+               'cloud': sat_args['cloud'],
+               'username': s2_credentials['username'],
                'password': s2_credentials['password'],
-               'path':args.path}
+               'path': sat_args['sat_path']}
 
     #download sentinel files
     s = download_sentinel.download_sentinel(**S2_args)
     s.download()
 
-elif args.sat == "Landsat8":
+elif sat_args['sat_type'] == "Landsat8":
 
     l8_credentials = config.landsat_pass
 
-    l8_args = {'inidate':sd,
-               'enddate':ed,
-               'region':args.region,
-               'coordinates': coord,
-               'producttype':'LANDSAT_8_C1',
-               'cloud': args.cloud,
-               'username':l8_credentials['username'],
+    l8_args = {'inidate': sd,
+               'enddate': ed,
+               'region': sat_args['region'],
+               'coordinates': sat_args['coordinates'],
+               'producttype': 'LANDSAT_8_C1',
+               'cloud': sat_args['cloud'],
+               'username': l8_credentials['username'],
                'password': l8_credentials['password'],
-               'path':args.path}
+               'path': sat_args['sat_path']}
 
     #download landsat files
     l = download_landsat.download_landsat(**l8_args)
     l.download()
 
-elif args.sat == 'All':
+elif sat_args['sat_type'] == 'All':
 
     #ESA credentials
     s2_credentials = config.sentinel_pass
 
-    S2_args = {'inidate':sd,
-               'enddate':ed,
-               'region':args.region,
-               'coordinates': coord,
-               'platform':'Sentinel-2',
-               'producttype':'S2MSI1C',
-               'cloud': args.cloud,
-               'username':s2_credentials['username'],
+    S2_args = {'inidate': sd,
+               'enddate': ed,
+               'region': sat_args['region'],
+               'coordinates': sat_args['coordinates'],
+               'platform': 'Sentinel-2',
+               'producttype': 'S2MSI1C',
+               'cloud': sat_args['cloud'],
+               'username': s2_credentials['username'],
                'password': s2_credentials['password'],
-               'path':args.path}
+               'path': sat_args['sat_path']}
 
     #download sentinel files
     s = download_sentinel.download_sentinel(**S2_args)
@@ -114,15 +79,15 @@ elif args.sat == 'All':
     #NASA credentials
     l8_credentials = config.landsat_pass
 
-    l8_args = {'inidate':sd,
-               'enddate':ed,
-               'region':args.region,
-               'coordinates': coord,
-               'producttype':'LANDSAT_8_C1',
-               'cloud': args.cloud,
-               'username':l8_credentials['username'],
+    l8_args = {'inidate': sd,
+               'enddate': ed,
+               'region': sat_args['region'],
+               'coordinates': sat_args['coordinates'],
+               'producttype': 'LANDSAT_8_C1',
+               'cloud': sat_args['cloud'],
+               'username': l8_credentials['username'],
                'password': l8_credentials['password'],
-               'path':args.path}
+               'path': sat_args['sat_path']}
 
     #download landsat files
     l = download_landsat.download_landsat(**l8_args)
